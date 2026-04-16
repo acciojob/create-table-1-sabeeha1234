638f8ce227f0a6b9cc6e1d16febbc9e77eb71b5c
sabeeha shaik <142984283+sabeeha1234@users.noreply.github.com>
2026-04-16 10:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
     //Write your code here
+	const table = document.getElementById("sampleTable")
+
+    const newRow = document.createElement("tr")
+
+    const cell1 = document.createElement("td")
+    const cell2 = document.createElement("td")
+
+    cell1.innerText = "New Cell1"
+    cell2.innerText = "New Cell2"
+
+    newRow.appendChild(cell1)
+    newRow.appendChild(cell2)
+
+    // insert at top
+    table.insertBefore(newRow, table.firstChild)
   
   
 }
